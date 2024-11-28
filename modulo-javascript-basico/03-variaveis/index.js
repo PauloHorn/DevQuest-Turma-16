75678351151ad56969dfba7f9060bd3c6dfa22fb
Paulo Horn <paulovictorhorn@gmail.com>
2024-11-27 21:05:10 -0300

    variaveis-em-JS

---
+++ modulo-javascript-basico/03-variaveis/index.js
@@ -0,0 +1,12 @@
+var nomePessoa = prompt("Digite seu nome:");
+
+const emailPessoa = prompt("Digite seu e-mail");
+
+let telefonePessoa = prompt("Digite seu telefone");
+
+let gostando = confirm("Está gostando do curso?");
+
+var teste = true;
+
+console.log("Teste");
+console.log(teste);
