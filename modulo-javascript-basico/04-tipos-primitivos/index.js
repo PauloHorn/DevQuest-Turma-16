5a038167dc0e92f9a0a03dd8094119081c6ea240
Paulo Horn <paulovictorhorn@gmail.com>
2024-11-28 21:07:15 -0300

    tipos-primitivos-do-js

---
+++ modulo-javascript-basico/04-tipos-primitivos/index.js
@@ -0,0 +1,7 @@
+let nome = "Paulo"; //string
+let numero = 1.23;//number - decimais são separados com ponto, podem ser negativos também
+let usuarioVerificado = true;//boolean
+let pessoa = null; //null
+let telefone; //undefined
+
+console.log(telefone);
