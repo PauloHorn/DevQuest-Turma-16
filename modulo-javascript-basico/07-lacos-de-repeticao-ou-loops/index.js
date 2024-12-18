44a91f72e67195904bf9d746eca9cb4a0de2f857
Paulo Horn <paulovictorhorn@gmail.com>
2024-12-18 13:43:20 -0300

    lacos-de-repeticao-com-for

---
+++ modulo-javascript-basico/07-lacos-de-repeticao-ou-loops/index.js
@@ -0,0 +1,10 @@
+//for(inicializador; condição; incremento)
+//declaração
+
+
+//o código abaixo imprimi a tabuáda do 9
+for(let i = 0; i < 11; i++) {
+    console.log ("9 x " + i + " = " + 9 * i)
+};
+
+//i++ é o mesmo que i = i + 1
