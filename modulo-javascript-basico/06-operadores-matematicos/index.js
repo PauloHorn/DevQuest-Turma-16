c99b5ec81574c58fc1509f906bb4f4586cb2de96
Paulo Horn <paulovictorhorn@gmail.com>
2024-12-18 13:25:06 -0300

    operadores-matematicos

---
+++ modulo-javascript-basico/06-operadores-matematicos/index.js
@@ -0,0 +1,12 @@
+var primeiroValor = 4;
+var segundoValor = 2;
+
+console.log(primeiroValor + segundoValor);// Exibi no concole a soma
+
+console.log(primeiroValor - segundoValor);// Exibi no concole a subtração
+
+console.log(primeiroValor * segundoValor);// Exibi no concole a multiplicação
+
+console.log(primeiroValor / segundoValor);// Exibi no concole a divisão
+
+console.log(primeiroValor % segundoValor);// Exibi no concole o resto da divisão
