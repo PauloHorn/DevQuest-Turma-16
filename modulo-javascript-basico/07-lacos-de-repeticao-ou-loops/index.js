3fe4638e62d910ef503d90604f2ab24d76bdfdcd
Paulo Horn <paulovictorhorn@gmail.com>
2024-12-19 15:04:36 -0300

    comando-while-e-do-while

--- modulo-javascript-basico/07-lacos-de-repeticao-ou-loops/index.js
+++ modulo-javascript-basico/07-lacos-de-repeticao-ou-loops/index.js
@@ -4,7 +4,29 @@
 
 //o código abaixo imprimi a tabuáda do 9
 for(let i = 0; i < 11; i++) {
-    console.log ("9 x " + i + " = " + 9 * i)
+    console.log ("9 x " + i + " = " + 9 * i);
 };
 
 //i++ é o mesmo que i = i + 1
+
+
+//while(condição) {
+// bloco de execução
+//}
+
+//o código abaixo exibe a tabuada do 8
+let a = 0;
+while(a < 11) {
+    console.log("8 x " + a + " = " + 8 * a);
+    a++;
+};
+
+
+//do{
+//bloco de execeução
+//} while(condição)
+//o código abaixo exibe a tabuada do 7
+ var i = 0;
+ do{
+    console.log("7 x " + i + " = " + 7 * i);
+ }while (i < 11);
