7f67e87b0a1518b3a64cee6ff45077e3d2962c2c
Paulo Horn <paulovictorhorn@gmail.com>
2025-02-08 09:48:03 -0300

    erros-em-js-no-console

---
+++ modulo-javascript-intermediario/20-resolvendo-erros-com-js/index.js
@@ -0,0 +1,13 @@
+// let valorBoleto = 100;
+
+// console.log(valorBoleto);
+
+ let inputTelefone = document.getElementById("telefone");
+
+ inputTelefone.disabled = true;
+
+ console.log(inputTelefone);
+
+function habilitarTelefone() {
+    inputTelefone.disabled = false;
+};
