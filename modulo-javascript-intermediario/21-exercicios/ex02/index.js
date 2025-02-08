556336055281fb483dcaf5d6938e9b889473bfe6
Paulo Horn <paulovictorhorn@gmail.com>
2025-02-08 15:57:55 -0300

    ex2-forEach-e-map

---
+++ modulo-javascript-intermediario/21-exercicios/ex02/index.js
@@ -0,0 +1,10 @@
+let numeros = [65, 44, 12, 4];
+
+numeros.forEach(numero => console.log(numero * 10));
+
+
+let numeros2 = [65, 44, 12, 4];
+
+numeros2MultiplicadosPor10 = numeros2.map(numero => numero * 10);
+
+console.log(numeros2MultiplicadosPor10);
