8e70c1fa6fb1695973c563d36c1b61e37b29dcdd
Paulo Horn <paulovictorhorn@gmail.com>
2025-02-08 16:03:51 -0300

    testando

--- modulo-javascript-intermediario/21-exercicios/ex02/index.js
+++ modulo-javascript-intermediario/21-exercicios/ex02/index.js
@@ -8,3 +8,16 @@ let numeros2 = [65, 44, 12, 4];
 numeros2MultiplicadosPor10 = numeros2.map(numero => numero * 10);
 
 console.log(numeros2MultiplicadosPor10);
+
+let paragrafos = document.getElementsByTagName("p");
+
+console.log(paragrafos);
+
+let paragrafo1 = paragrafos[0];
+
+console.log(paragrafo1);
+
+function trocarCor() {
+    paragrafo1.style.backgroundColor = "red";
+}
+
