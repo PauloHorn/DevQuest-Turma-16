0e5cb7c1eaf8ad63270277a4bf769e8d739b922c
Paulo Horn <paulovictorhorn@gmail.com>
2025-02-19 22:28:35 -0300

    js-finalizado-e-funcionando-melhorar-css

--- modulo-javascript-intermediario/21-exercicios/desafio-js/src/js/validacao.js
+++ modulo-javascript-intermediario/21-exercicios/desafio-js/src/js/validacao.js
@@ -1,3 +1,20 @@
 const botaoEnviar = document.getElementById("btn-enviar");
 
-botaoEnviar.addEventListener("click", function () {});
+
+botaoEnviar.addEventListener("click", function () {
+    const inputText = document.querySelectorAll(".input-text");
+    const campoObrigatorio = document.querySelectorAll(".campo-obrigatorio");
+    let contador = 0;
+
+    inputText.forEach(input => {
+        if(input.value === "") {
+            input.classList.add("sem-preenchimento");
+            campoObrigatorio[contador].classList.add("mostrar");
+            contador++;
+        } else {
+            input.classList.add("preenchido");
+            campoObrigatorio[contador].classList.remove("mostrar");
+            contador++;
+        };
+    });
+});
