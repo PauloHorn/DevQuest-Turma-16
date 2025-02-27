5660d37b3fa6ac2c4958ee3b859af55f05235d48
Paulo Horn <paulovictorhorn@gmail.com>
2025-02-26 21:56:19 -0300

    desestruturacao-de-objetos-e-arrays

---
+++ modulo-javascrip-avancado/05-desestruturacao-de-objetos-e-arrays/index.js
@@ -0,0 +1,41 @@
+//Em arrays:
+
+const numeros = [1, 2, 3, 4, 5];
+
+//Na maneira antiga para extraírmos cada um dos valores, faríamos:
+
+// const primeiro = numeros[0];
+// const segundo = numeros[1];
+
+
+// Com a desestruturação com nomes de variáveis correspondentes à posição do elemento
+
+const [primeiro, segundo, terceiro, quarto = 0] = numeros;
+
+console.log(primeiro); // 1
+console.log(segundo); // 2
+console.log(terceiro); // 3
+console.log(quarto); // 4
+
+// Em objetos:
+
+let pessoa = {
+    nome: "Paulo",
+    sobrenome: "Horn",
+    idade: 38
+};
+
+// let {nome: nome, idade: idade, sobrenome: sobrenome} = pessoa;
+
+// console.log(nome);
+// console.log(idade);
+// console.log(sobrenome);
+
+
+//podemos reumir mais, pois quando estamos criando as variáveis, como elas tem o mesmo nome dos componentes do objeto, podemos simplificar:
+
+let {nome, idade, sobrenome} = pessoa;
+
+console.log(nome);
+console.log(idade);
+console.log(sobrenome);
