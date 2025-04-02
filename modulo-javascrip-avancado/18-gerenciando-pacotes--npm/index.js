80978d48bb03b6cce83ff7e08c176eb924711bfa
Paulo Horn <paulovictorhorn@gmail.com>
2025-04-01 21:48:32 -0300

    export-e-import

--- modulo-javascrip-avancado/18-gerenciando-pacotes--npm/index.js
+++ modulo-javascrip-avancado/18-gerenciando-pacotes--npm/index.js
@@ -1,4 +1,6 @@
 const kindof = require("kind-of");
 const testeDeBooleano = true;
+const variavelDeTeste = 34;
 console.log(kindof(true));
 console.log(testeDeBooleano);
+console.log(kindof(variavelDeTeste));
---
+++ modulo-javascrip-avancado/19-modulos-em-javascript/src/javascript/operacoes-matematicas.js
@@ -0,0 +1,11 @@
+export function somar (numero1, numero2) {
+    return numero1 + numero2;
+};
+
+export function multiplicar (numero1,numero2) {
+    return numero1*numero2;
+};
+
+
+//o ideal é usar o export de uma vez no final do arquivo como mostrado abaixo
+//export {somar, multiplicar};
